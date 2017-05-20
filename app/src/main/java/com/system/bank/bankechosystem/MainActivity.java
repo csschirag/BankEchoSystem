9ed75649d74cff36b4bdb7ac7b5e33e8b08e6914
Chirag Sharma <csschirag77@gmail.com>
2017-05-20 21:55:02 +0530

    API.AI

--- app/src/main/java/com/system/bank/bankechosystem/MainActivity.java
+++ app/src/main/java/com/system/bank/bankechosystem/MainActivity.java
@@ -9,34 +9,43 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.speech.RecognizerIntent;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.system.bank.bankechosystem.api.ai.ApiAiServiceProvider;
+
 import java.util.ArrayList;
 import java.util.Locale;
 
+import ai.api.AIListener;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity implements AIListener {
     private static final int REQ_CODE_SPEECH_INPUT = 1000;
-    GoogleTranslate translator;
-    EditText translateedittext;
-    TextView translatabletext;
-    Button mSpeak;
+    private GoogleTranslate translator;
+    private EditText translateedittext;
+    private TextView translatabletext;
+    private Button mSpeak, mAPIAIBtn;
+    private ApiAiServiceProvider mServiceProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mServiceProvider = new ApiAiServiceProvider(this);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         translateedittext = (EditText) findViewById(R.id.translateedittext);
         translatabletext = (TextView) findViewById(R.id.translatabletext);
         mSpeak = (Button) findViewById(R.id.speak);
+        mAPIAIBtn = (Button) findViewById(R.id.api_list);
         Button translatebutton = (Button) findViewById(R.id.translatebutton);
         translatebutton.setOnClickListener(new View.OnClickListener() {
 
@@ -53,8 +62,14 @@ public class MainActivity extends ActionBarActivity {
                 promptSpeechInput();
             }
         });
+        mAPIAIBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mServiceProvider.setAiListener(MainActivity.this);
+                mServiceProvider.getAiService().startListening();
+            }
+        });
     }
-
 
     private class EnglishToTagalog extends AsyncTask<Void, Void, Void> {
         private ProgressDialog progress = null;
@@ -104,6 +119,7 @@ public class MainActivity extends ActionBarActivity {
 
             super.onPostExecute(result);
             translatabletext.setText(translated(textToConvert));
+
         }
 
         @Override
@@ -113,6 +129,26 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // use this method to disconnect from speech recognition service
+        // Not destroying the SpeechRecognition object in onPause method would block other apps from using SpeechRecognition service
+        if (mServiceProvider.getAiService() != null) {
+            mServiceProvider.getAiService().pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // use this method to reinit connection to recognition service
+        if (mServiceProvider.getAiService() != null) {
+            mServiceProvider.getAiService().resume();
+        }
+    }
 
     public String translated(String input){
         String text = translator.translte(input, "hi", "en");
@@ -148,5 +184,35 @@ public class MainActivity extends ActionBarActivity {
 
                 break;
         }
+    }
+
+    @Override
+    public void onResult(AIResponse result) {
+        Log.e(getClass().getSimpleName(),"onResult");
+    }
+
+    @Override
+    public void onError(AIError error) {
+        Log.e(getClass().getSimpleName(),"onError");
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+        Log.e(getClass().getSimpleName(),"onAudioLevel");
+    }
+
+    @Override
+    public void onListeningStarted() {
+        Log.e(getClass().getSimpleName(),"onListeningStarted");
+    }
+
+    @Override
+    public void onListeningCanceled() {
+        Log.e(getClass().getSimpleName(),"onListeningCanceled");
+    }
+
+    @Override
+    public void onListeningFinished() {
+        Log.e(getClass().getSimpleName(),"onListeningFinished");
     }
 }
---
+++ app/src/main/java/com/system/bank/bankechosystem/helper/Constants.java
@@ -0,0 +1,10 @@
+package com.system.bank.bankechosystem.helper;
+
+/**
+ * Created by B0097489 on 5/20/17.
+ */
+
+public class Constants {
+
+    public static final String API_AI_KEY = "24a4e1040e3848e7933199a93803fdc4";
+}
