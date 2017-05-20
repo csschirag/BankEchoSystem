70b9dac1bed611221088b31726220744cc5690eb
Chirag Sharma <csschirag77@gmail.com>
2017-05-20 23:34:26 +0530

    few changes

--- app/src/main/java/com/system/bank/bankechosystem/Registration.java
+++ app/src/main/java/com/system/bank/bankechosystem/Registration.java
@@ -1,10 +1,8 @@
 package com.system.bank.bankechosystem;
 
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class Registration extends AppCompatActivity {
 
@@ -14,15 +12,6 @@ public class Registration extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
 }
